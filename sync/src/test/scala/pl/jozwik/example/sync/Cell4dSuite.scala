c9f6d5ec7c404cc74cb855784c0467ef2b7da43a
andrzej.jozwik@gmail.com <andrzej.jozwik@gmail.com>
2019-08-21 12:56:42 +0200

    Adapted to 0.8.1

--- sync/src/test/scala/pl/jozwik/example/sync/Cell4dSuite.scala
+++ sync/src/test/scala/pl/jozwik/example/sync/Cell4dSuite.scala
@@ -3,7 +3,7 @@ package pl.jozwik.example.sync
 import pl.jozwik.example.domain.model.{ Cell4d, Cell4dId }
 import pl.jozwik.example.repository.Cell4dRepositoryGen
 import pl.jozwik.quillgeneric.quillmacro.sync.SyncRepository
-
+import org.scalatest.TryValues._
 import scala.util.Success
 
 trait Cell4dSuite extends AbstractSyncSpec {
@@ -14,7 +14,11 @@ trait Cell4dSuite extends AbstractSyncSpec {
         val id     = Cell4dId(0, 1, 0, 1)
         val entity = Cell4d(id, false)
         repository.createOrUpdateAndRead(entity) shouldBe Success(entity)
+        repository.readUnsafe(id).success.get shouldBe entity
         repository.delete(id) shouldBe Success(1)
+        intercept[RuntimeException] {
+          repository.readUnsafe(id).success.get
+        }
       }
     }
 }
